4c6abdfd052bced63acfbdaba1588e47425715d9
Karl Waugh <karlmvwaugh@gmail.com>
2020-02-08 23:19:34 +0000

    Update DebugVolume.cs
    Debug Volume

--- OutlineOfAGlassCave/Assets/DebugVolume.cs
+++ OutlineOfAGlassCave/Assets/DebugVolume.cs
@@ -2,10 +2,10 @@
 using System.Collections;
 
 public class DebugVolume : MonoBehaviour {
-
+	private SpriteRenderer renderer;
 	// Use this for initialization
 	void Start () {
-	
+		renderer = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
@@ -14,6 +14,8 @@ public class DebugVolume : MonoBehaviour {
 	}
 
 	public void Fire(float volume) {
-
+		var newVolume = Mathf.Pow(volume, 0.25f);
+		var currentScale = renderer.transform.localScale;
+		renderer.transform.localScale = new Vector3(currentScale.x, newVolume, currentScale.z);
 	}
 }
